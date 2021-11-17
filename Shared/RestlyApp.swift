5c9ca6c5364a95bfadb26051257875d3333431c0
Tim <0xtimc@gmail.com>
2021-11-17 17:59:35 +0000

    Initial project creation

---
+++ Shared/RestlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestlyApp.swift
+//  Shared
+//
+//  Created by Tim Condon on 17/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct RestlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
